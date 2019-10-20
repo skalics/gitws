dcd464d041f0739e7dad75a2133e0cd8eba61769
sebastian <sebastian@localhost>
2019-10-20 15:07:26 +0200

    first commit - do it

---
+++ src/at/java/allianz/demo/DemoPage.java
@@ -0,0 +1,12 @@
+package at.java.allianz.demo;
+import javax.swing.JOptionPane;
+
+public class DemoPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showConfirmDialog(null, "are you ready", "ask",1);
+
+	}
+
+}
